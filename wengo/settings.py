9b7be3d114def3983e6210d08a7d35bf0001e432
SivakumarATF <sivakumaratf@gmail.com>
2025-10-25 15:16:15 +0530

    1st live into server

--- wengo/settings.py
+++ wengo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9q*@q8m%wyaxj5d$u3w#ezzx5==%+x_gs*gu9sq%0g+8q7yafn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
