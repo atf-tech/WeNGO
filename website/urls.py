a140303d968b9b29e891ba80a2ab6392351cbbb9
SivakumarATF <sivakumaratf@gmail.com>
2026-03-17 17:11:13 +0530

    banners / footer

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('service', views.service, name='service'),
+    path('services', views.service, name='service'),
     path('join_us', views.join_us, name='join_us'),
     path('contact', views.contact, name='contact'),
     path('service_detail', views.service_detail, name='service_detail'),
