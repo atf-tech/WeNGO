34b929c22f9906eeb34c8189926ea944ef70a810
SivakumarSK3639 <karthi7708212@gmail.com>
2026-09-24 14:37:52 +0530

    SEO for 6 pages

--- dashboard/models.py
+++ dashboard/models.py
@@ -94,9 +94,13 @@ class Services(models.Model):
     slug = models.SlugField(unique=True, blank=True)
 
     def save(self, *args, **kwargs):
-
-        if not self.slug:
-            self.slug = slugify(self.service_name)  
+        base_slug = slugify(self.service_name)
+        slug = base_slug
+        counter = 1
+        while self.__class__.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        self.slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
